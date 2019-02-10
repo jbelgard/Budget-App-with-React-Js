b7018056efbdb6709af978290d430dc8cb62b519
Leonardo Yukdawan Liwan <35754346+leoliwan@users.noreply.github.com>
2019-02-10 13:55:25 +0700

    Update BalanceDisplay.js

--- src/display/BalanceDisplay.js
+++ src/display/BalanceDisplay.js
@@ -1,46 +1,33 @@
 import React from 'react';
-import { BudgetConsumer } from '../store'; 
 
 const BalanceDisplay = () => {
     return (
-        <BudgetConsumer>
-            {value => {
-                const totalExpense = value.expenses.length > 0 ? (
-                    value.expenses.reduce((acc, curr) => {
-                        acc += curr.amount
-                        return acc
-                    }, 0)) : 0;
-                
-                    return (
-                        <div className="row">
-                            <div className="col-lg-4">
-                                <div className="card">
-                                    <div className="card-header">Budget</div>
-                                    <div className="card-body">
-                                        <h5 className="text-center card-title">{value.budget}</h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4">
-                                <div className="card">
-                                    <div className="card-header">Expenses</div>
-                                    <div className="card-body">
-                                        <h5 className="text-center card-title">{totalExpense}</h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4">
-                                <div className="card">
-                                    <div className="card-header">Balance</div>
-                                    <div className="card-body">
-                                        <h5 className="text-center card-title">{value.budget - totalExpense}</h5>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-           }}
-        </BudgetConsumer>
+        <div className="row">
+            <div className="col-lg-4">
+                <div className="card">
+                    <div className="card-header">Budget</div>
+                    <div className="card-body">
+                        <h5 className="text-center card-title">{value.budget}</h5>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <div className="card">
+                    <div className="card-header">Expenses</div>
+                    <div className="card-body">
+                        <h5 className="text-center card-title">{totalExpense}</h5>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-4">
+                <div className="card">
+                    <div className="card-header">Balance</div>
+                    <div className="card-body">
+                        <h5 className="text-center card-title">{value.budget - totalExpense}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
