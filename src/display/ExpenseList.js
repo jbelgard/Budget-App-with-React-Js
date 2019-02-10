24e6df70f33d0dbd595a73f8f91e73cc2c1089cb
Leonardo Yukdawan Liwan <35754346+leoliwan@users.noreply.github.com>
2019-02-10 13:58:07 +0700

    Update ExpenseList.js

--- src/display/ExpenseList.js
+++ src/display/ExpenseList.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { BudgetConsumer } from '../store';
 
 class ExpenseList extends Component {
     render() {
@@ -12,26 +11,10 @@ class ExpenseList extends Component {
                             <th>Amount</th>
                         </tr>
                     </thead>
-                    <BudgetConsumer>
-                        {value => {
-                           const expensesList = value.expenses.length > 0 ? (
-                               value.expenses.map((expense, index) => {
-                                   return (
-                                       <tr key={index}>
-                                           <td>{expense.title}</td>
-                                           <td>{expense.amount}</td>
-                                       </tr>
-                                   )
-                               })
-                           ) : (
-                               <tr>
-                                   <td>No Listed Expenses Yet</td>
-                               </tr>
-                           ) 
-                           return <tbody>{expensesList}</tbody>
-                           
-                        }}
-                    </BudgetConsumer>
+                 <tbody>
+            
+                </tbody>
+                          
                 </table>
             </div>
         )
